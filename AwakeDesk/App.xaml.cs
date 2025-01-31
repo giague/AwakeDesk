97e38fa538bd0b4d39c645cc14cca70303699c98
Giovanni Guerra <giague@gmail.com>
2025-01-31 16:01:51 +0100

    Added single instance check

--- AwakeDesk/App.xaml.cs
+++ AwakeDesk/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Configuration;
 using System.Data;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows;
 
 namespace AwakeDesk
@@ -11,23 +12,29 @@ namespace AwakeDesk
     /// </summary>
     public partial class App : Application
     {
+        private static Mutex mutex = new Mutex(true, "AwakeDeskApp");
+
         [DllImport("kernel32.dll", SetLastError = true)]
         static extern uint SetThreadExecutionState(uint esFlags);
         public static AwakeDeskSettings AwakeDeskSettings { get; private set; }
         public static AwakeVariables AwakeVariables { get; private set; }
 
-        private void Application_Startup(object sender, StartupEventArgs e)
+        protected override void OnExit(ExitEventArgs e)
         {
-            SetThreadExecutionState(0x80000002);
+            mutex.ReleaseMutex();
+            SetThreadExecutionState(0x80000000);
+            base.OnExit(e);
         }
 
-        private void Application_Exit(object sender, ExitEventArgs e)
-        {
-            SetThreadExecutionState(0x80000000);
-        }
         protected override void OnStartup(StartupEventArgs e)
         {
+            if (!mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.Current.Shutdown();
+                return;
+            }
             base.OnStartup(e);
+            SetThreadExecutionState(0x80000002);
             AwakeDeskSettings = new();
             AwakeDeskSettings.LoadFromConfiguration();
             AwakeVariables = new();
